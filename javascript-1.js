04ca5f0d082d01cb08f8d5fdbf3e79c323dd6b66
i.kapustkin <54114611+ximera405@users.noreply.github.com>
2020-04-05 13:47:47 +0300

    Rework task

--- javascript-1.js
+++ javascript-1.js
@@ -1,6 +1,24 @@
+// Множественное использование
 var prevSum = 0;
+let sum = (x) => {
+  if (x == undefined) return prevSum;
+  let summ = x;
+  return (y) => {
+    if (y !== undefined) {
+      return sum(summ + y)
+    } else {
+      prevSum = summ;
+      return summ;
+    }
+  }
+}
+console.log(sum(1)(2)(3)(4)());
+console.log(sum())
 
-let sum = (...n) => {
+
+// Сумма всех аргуметов
+var prevSum1 = 0;
+let sum1 = (n)  => {
   let summ = 0;
   if (n.length !== 0) {
     for (let i = 0; i < n.length; i++) {
