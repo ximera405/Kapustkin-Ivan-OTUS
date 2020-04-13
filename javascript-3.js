b3610b5fd7a3e186c61ff3e2b4cca16e71241139
i.kapustkin <54114611+ximera405@users.noreply.github.com>
2020-04-13 14:13:39 +0300

    Reword js-3

--- javascript-3.js
+++ javascript-3.js
@@ -1,15 +1,23 @@
 'use strict';
 
-const getPath = (currentElement) => {
-  const currentClassName = document.querySelectorAll(`.${currentElement.className}`);
-  if (currentClassName.length == 1) {
-    console.log(`${currentElement.className} is unique selector`)
-    return currentElement.className
+const getPath = (currentElement, uniqueName = '') => {
+  let number = 0;
+
+  for (let i = 0; i < currentElement.parentElement.childElementCount; i++) {
+    console.log(document.body.children[i]);
+    if (currentElement === currentElement.parentElement.children[i]) {
+      number = i + 1;
+    }
   }
-  else if (currentClassName.length == undefined || currentClassName.length == 0 || currentClassName.length == null) {
-    console.log(`Element doesn't have a className`)
+
+  if (number !== 0) {
+    uniqueName += `${currentElement.localName}:nth-child(${number})`;
   }
-  else {
-    console.log(`DOM contains 2 or more selected classNames`)
+
+  if (currentElement.parentElement.localName !== "body") {
+    uniqueName += ' ';
+    return getPath(currentElement.parentElement, uniqueName);
   }
+
+  return `body ${uniqueName}`;
 }
