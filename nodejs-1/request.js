6f3da229bbc7f06ed73da4c7f6d0e5b1bab48949
i.kapustkin <54114611+ximera405@users.noreply.github.com>
2020-04-24 10:10:54 +0300

    Rework requests to console

--- nodejs-1/request.js
+++ nodejs-1/request.js
@@ -1,35 +1,65 @@
-const count = document.querySelector(".counter-number");
-const parrarel = document.getElementById("par");
-const posled = document.getElementById("pos");
+console.log(`\nHow to start server : 
+"node request.js " + (Кол-во запросов)+ " " + (1 - последовательно, 2 - параллельно)\n`)
 
-const sender = document.querySelector(".send-request");
+const http = require('http');
 
-const parReq = (i) => {
-  const xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "http://localhost:3000/", true); // false for synchronous request
-  xmlHttp.send(null);
-  if (i != count.value) {
-    parReq(i + 1);
+const reqCount = process.argv[2];
+const typeOfRequests = process.argv[3];
+
+const options = {
+  port: 3000,
+  hostname: 'localhost',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'text/plain',
   }
-  return;
+};
+
+const req = function(x = 1) {
+  http.get(options, (resp) => {
+    let data = '';
+  
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.info(`Server response: ${data}`);
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
+  })
+  .on("close", () => {
+    if (x < reqCount) {
+      req(x + 1);
+    }
+  })
 }
 
-const posReq = (i) => {
-  const xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "http://localhost:3000/", false); // false for synchronous request
-  xmlHttp.send(null);
-  if (i != count.value) {
-    posReq(i + 1);
+if (reqCount > 0) {
+  if (typeOfRequests == 2) {
+    for (let i = 0; i < reqCount; i++) {
+      http.get(options, (resp) => {
+        let data = '';
+      
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          console.info(`Server response: ${data}`);
+        });
+        }).on("error", (err) => {
+          console.log("Error: " + err.message);
+      })
+    }
   }
-  return;
+  else {
+    req();
+  }
 }
-
-sender.addEventListener("click", (e) => {
-  e.preventDefault();
-  let i = 1;
-  if (posled.checked) {
-    posReq(i)
-  } else if (parrarel.checked) {
-    parReq(i);
-  }
-});
